097f48ada748715e78f5f386a3a98d42bfc54927
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-18 17:22:22 +0700

    điểm thi

---
+++ src/model/Scores.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.io.Serializable;
+
+public class Scores implements Serializable {
+    private double mathScores;
+    private double physicalScore;
+    private double chemistryScore;
+
+    public Scores() {
+    }
+
+    public Scores(double mathScores, double physicalScore, double chemistryScore) {
+        this.mathScores = mathScores;
+        this.physicalScore = physicalScore;
+        this.chemistryScore = chemistryScore;
+    }
+
+    public double getMathScores() {
+        return mathScores;
+    }
+
+    public void setMathScores(double mathScores) {
+        this.mathScores = mathScores;
+    }
+
+    public double getPhysicalScore() {
+        return physicalScore;
+    }
+
+    public void setPhysicalScore(double physicalScore) {
+        this.physicalScore = physicalScore;
+    }
+
+    public double getChemistryScore() {
+        return chemistryScore;
+    }
+
+    public void setChemistryScore(double chemistryScore) {
+        this.chemistryScore = chemistryScore;
+    }
+
+    // điểm trung bình
+    public double averageOfSubject(){
+       double average= (this.getMathScores()+this.getPhysicalScore()+this.getChemistryScore())/3;
+        return average;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                " Môn toán: " + mathScores +
+                ", Môn lý: " + physicalScore +
+                ", Môn hóa: " + chemistryScore +
+                ", Điểm trung bình: "+this.averageOfSubject()+
+                "]";
+    }
+}
