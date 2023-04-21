00d4b66babd636bc22718bcb2b88ecb0c08bc435
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-21 15:03:20 +0700

    giao diện

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Student student=new Student("1","Nguyễn Văn A", Student.Gender.Nam, LocalDate.of(1996,12,26),"0123456789",new Address("1A","HN","HN","HN"),"a@gmailk.com",new Scores(8,9,6));
-//        Student student1=new Student("2","Hoàng Văn B", Student.Gender.Nam, LocalDate.of(1996,12,26),"123456789",new Address("1A","HN","HN","HN"),"a@gmailk.com",new Scores(1,2,3));
-//        Student student2=new Student("3","Nguyễn Thị B", Student.Gender.Nữ, LocalDate.of(1996,12,26),"1234567890",new Address("1A","HN","HN","HN"),"a@gmailk.com",new Scores(1,2,3));
-//////        System.out.println(student);
-//////        System.out.println(student1);
+//        Student student=new Student("SV000001","Nguyễn Văn Anh", Student.Gender.Nam, LocalDate.of(1994,12,20),"(+84)-(012345678)",new Address("1A","Tam Trinh","Hoàng Mai","Hà Nội"),"a@gmailk.com",new Scores(8,9,6));
+//        Student student1=new Student("SV000002","Đặng Văn Hoàng", Student.Gender.Nam, LocalDate.of(1993,12,12),"(+84)-(555555555)",new Address("2B","Đình Thôn","Mỹ Đình","Hà Nội"),"b@gmailk.com",new Scores(4,2,5));
+//        Student student2=new Student("SV000003","Nguyễn Thị Hồng", Student.Gender.Nữ, LocalDate.of(1996,12,15),"(+84)-(666666666)",new Address("3","Từ Liêm","Mỹ ĐÌnh","Hà Nam"),"c@gmailk.com",new Scores(1,2,3));
+//        Student student3=new Student("SV000004","Trần Hoa Hồng", Student.Gender.Nữ, LocalDate.of(1992,12,15),"(+84)-(888888888)",new Address("5A","Từ Liêm","Mỹ ĐÌnh","Hà Nam"),"c@gmailk.com",new Scores(8,9,10));
 //       Manage manage= new Manage();
 //       manage.addStudent(student);
 //        manage.addStudent(student1);
 //        manage.addStudent(student2);
+//        manage.addStudent(student3);
 ////        //manage.displayStudent();
 //       //manage.studentClassification();
 //        //manage.sortScores();
@@ -28,15 +28,21 @@ public class Main {
         int choice=0;
         do {
             System.out.println("""
-                    | 0.Thoát chương trình.\n
-                    | 1.Thêm sinh viên.\n
-                    | 2.Xóa sinh viên.\n
-                    | 3.Sửa thông tin sinh viên.\n
-                    | 4.Hiển thị sinh viên theo tên.\n
-                    | 5.Hiển thị sinh viên theo điểm .\n
-                    | 6.Phân loại học lực sinh viên.\n
-                    | 7.Tìm kiếm sinh viên theo mã sinh viên.\n
-                    | 8.Tìm kiếm theo tên.\n
+                    ***************************************************
+                    ***************************************************
+                    **                                               **
+                    **     0.Thoát chương trình.                     **
+                    **     1.Thêm sinh viên.                         **
+                    **     2.Xóa sinh viên.                          **
+                    **     3.Sửa thông tin sinh viên.                **
+                    **     4.Hiển thị sinh viên theo tên.            **
+                    **     5.Hiển thị sinh viên theo điểm .          **
+                    **     6.Phân loại học lực sinh viên.            **
+                    **     7.Tìm kiếm sinh viên theo mã sinh viên.   **
+                    **     8.Tìm kiếm theo tên.                      **
+                    **                                               **
+                    ***************************************************
+                    ***************************************************
                     """);
             System.out.println();
             System.out.println("Vui lòng chọn chức năng !!!");
