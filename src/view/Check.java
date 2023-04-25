9ea93eef14318994784390e8db3d326e1c8fae7f
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-25 16:56:05 +0700

    định dạng regex

--- src/view/Check.java
+++ src/view/Check.java
@@ -9,7 +9,21 @@ public class Check {
         private static final String email="^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
         private static final String phone="^\\(\\+84\\)-\\(\\d{9}\\)$";
         private static final String id="^SV\\d{6}$";
+        private static final String name="^[a-zA-Z\\p{L}' ]{2,}(\\s[a-zA-Z\\p{L}' ]{2,})+$";
+        private static final String password="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
         public Check(){
+        }
+
+        public boolean checkPassword(String regex){
+            Pattern pattern=Pattern.compile(password);
+            Matcher matcher=pattern.matcher(regex);
+            return matcher.matches();
+        }
+
+        public boolean checkName(String regex){
+            Pattern pattern=Pattern.compile(name);
+            Matcher matcher=pattern.matcher(regex);
+            return matcher.matches();
         }
         public boolean checkId(String regex){
             Pattern pattern= Pattern.compile(id);
